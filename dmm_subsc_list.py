a52079e381f9052a4f6d70ba0004225a36b28d3c
PC-CNT <49097489+PC-CNT@users.noreply.github.com>
2023-09-10 10:07:34 +0900

    wkrn

--- dmm_subsc_list.py
+++ dmm_subsc_list.py
@@ -1,11 +1,20 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 import urllib
 import time
 import os
+# import collections
+# import operator
+# import argparse
 
 base_url = "https://dlsoft.dmm.co.jp/subsc/li/?page="
+
+
+HEADERS = {'user-agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+                           'AppleWebKit/537.36 (KHTML, like Gecko)'
+                           'Chrome/100.0.4765.0 Safari/537.36'),}
 
 dmm_cookie = {"age_check_done": "1"}
 
@@ -49,21 +58,21 @@ def conv_dic(soup):
 
 
 def main():
-    HEADERS = {'user-agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-                              'AppleWebKit/537.36 (KHTML, like Gecko)'
-                              'Chrome/100.0.4765.0 Safari/537.36'),}
+
     p = 1
     output_dic = {}
+    session = HTMLSession()
     while True:
-        page = requests.get(base_url + str(p), cookies=dmm_cookie, headers=HEADERS)
+        page = session.get(base_url + str(p), cookies=dmm_cookie, headers=HEADERS)
         print(f"Getting page:{p}…")
         if page.status_code == 200:
-            htmldata = page.text
-            soup = BeautifulSoup(htmldata, "html.parser")
+            # htmldata = page.text
+            soup = BeautifulSoup(page.text, "html.parser")
+            print(soup)
             dic_temp = conv_dic(soup)
             if dic_temp:
                 output_dic = dict(**output_dic, **dic_temp)
-                time.sleep(0.8)
+                time.sleep(1)
             else:
                 break
         elif page.status_code == 404:
@@ -79,35 +88,7 @@ def main():
     print("Done.")
 
 
-def debug():
-    with open("dmm_subsc_list_1.html", "r") as f:
-        htmldata = f.read()
-    soup = BeautifulSoup(htmldata, "html.parser")
-
-    subscListCards = soup.find("div", class_="subscListCards")
-
-    titles = soup.find_all("div", class_="component-list__title")
-    makers = soup.find_all("div", class_="component-list__maker")
-    links = subscListCards.find_all("a",)
-
-    list_title = [title.get_text() for title in titles]
-    list_maker = [maker.get_text() for maker in makers]
-    list_link = [urllib.parse.urljoin("https://dlsoft.dmm.co.jp/" ,link.get("href")) for link in links]
-    keys = ["title", "maker", "link"]
-    output = {
-        title: dict(zip(keys, [title, maker, link]))
-        for title, maker, link in zip(
-            list_title,
-            list_maker,
-            list_link
-        )
-    }
-    print(output)
-    with open("dmm_subsc_list_1.json", "w") as f:
-        json.dump(output, f, indent=4, ensure_ascii=False)
-
 
 
 if __name__ == "__main__":
     main()
-    # debug()
